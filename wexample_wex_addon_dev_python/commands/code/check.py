74abc354b7e00a461a9d3d9acef562d4da821e19
weeger <romain.weeger@gmail.com>
2025-06-08 23:45:32 +0200

    Minor

--- wexample_wex_addon_dev_python/commands/code/check.py
+++ wexample_wex_addon_dev_python/commands/code/check.py
@@ -31,10 +31,6 @@ def python__code__check(
         tool: Optional[str] = None,
         stop_on_failure: bool = True
 ) -> bool:
-    from wexample_wex_addon_dev_python.commands.code.check.mypy import _code_check_mypy
-    from wexample_wex_addon_dev_python.commands.code.check.pylint import _code_check_pylint
-    from wexample_wex_addon_dev_python.commands.code.check.pyright import _code_check_pyright
-
     """Check a Python file using various code quality tools.
 
     Args:
@@ -44,6 +40,10 @@ def python__code__check(
     Returns:
         bool: True if all checks pass, False otherwise
     """
+    from wexample_wex_addon_dev_python.commands.code.check.mypy import _code_check_mypy
+    from wexample_wex_addon_dev_python.commands.code.check.pylint import _code_check_pylint
+    from wexample_wex_addon_dev_python.commands.code.check.pyright import _code_check_pyright
+
     if not os.path.exists(file_path):
         kernel.io.error(f"Error: File {file_path} does not exist")
         return False
