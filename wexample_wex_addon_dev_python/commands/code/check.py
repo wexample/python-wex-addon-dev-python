29afcc918f5d4bf7cc7fd0e3f48aaa90041f76d1
weeger <romain.weeger@gmail.com>
2025-06-08 18:03:59 +0200

    File argument

--- wexample_wex_addon_dev_python/commands/code/check.py
+++ wexample_wex_addon_dev_python/commands/code/check.py
@@ -1,9 +1,15 @@
 from wexample_wex_core.common.kernel import Kernel
 from wexample_wex_core.decorator.command import command
+from wexample_wex_core.decorator.option import option
 
 
+@option(name="file_path", type=str, required=True)
 @command()
 def python__code__check(
-        kernel: "Kernel"
+        kernel: "Kernel",
+        file_path: str
 ) -> None:
-    print('Code OK')
+    import os
+
+    if os.path.exists(file_path):
+        print('Code OK ' + file_path)
