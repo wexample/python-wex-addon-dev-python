daea1074f8456efb7100e8eda0d925e6f38f2f4c
weeger <romain.weeger@gmail.com>
2025-06-09 16:56:25 +0200

    Register python middleware

---
+++ wexample_wex_addon_dev_python/middleware/each_python_file_middleware.py
@@ -0,0 +1,5 @@
+from wexample_wex_core.middleware.each_file_middleware import EachFileMiddleware
+
+
+class EachPythonFileMiddleware(EachFileMiddleware):
+    pass
--- wexample_wex_addon_dev_python/python_addon_manager.py
+++ wexample_wex_addon_dev_python/python_addon_manager.py
@@ -1,5 +1,13 @@
+from typing import List, Type
+
 from wexample_wex_core.common.abstract_addon_manager import AbstractAddonManager
+from wexample_wex_core.middleware.abstract_middleware import AbstractMiddleware
 
 
 class PythonAddonManager(AbstractAddonManager):
-    pass
+    def get_middlewares_classes(self) -> List[Type["AbstractMiddleware"]]:
+        from wexample_wex_addon_dev_python.middleware.each_python_file_middleware import EachPythonFileMiddleware
+
+        return [
+            EachPythonFileMiddleware,
+        ]
