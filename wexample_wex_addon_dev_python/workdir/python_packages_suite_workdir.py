8bbe92f9d4c007d9613a90db07117a6afc09f9f9
weeger <romain.weeger@gmail.com>
2025-08-22 15:27:37 +0200

    Pip suite

--- wexample_wex_addon_dev_python/workdir/python_packages_suite_workdir.py
+++ wexample_wex_addon_dev_python/workdir/python_packages_suite_workdir.py
@@ -1,9 +1,41 @@
 from __future__ import annotations
 
+from pathlib import Path
+
+from wexample_config.const.types import DictConfig
 from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
 
 
 class PythonPackagesSuiteWorkdir(FrameworkPackageSuiteWorkdir):
-    pass
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+        from wexample_filestate.config_option.children_filter_config_option import (
+            ChildrenFilterConfigOption,
+        )
+        from wexample_filestate.const.disk import DiskItemType
+        from wexample_wex_addon_dev_python.workdir.python_package_workdir import (
+            PythonPackageWorkdir,
+        )
+
+        raw_value = super().prepare_value(raw_value=raw_value)
+
+        children = raw_value["children"]
+
+        # By default, consider each sub folder as a pip package
+        children.extend(
+            [
+                ChildrenFilterConfigOption(
+                    filter=self._has_pyproject,
+                    pattern={
+                        "class": PythonPackageWorkdir,
+                        "type": DiskItemType.DIRECTORY,
+                    },
+                )
+            ]
+        )
+
+        return raw_value
+
+    def _has_pyproject(self, entry: Path) -> bool:
+        return entry.is_dir() and (entry / "pyproject.toml").is_file()
