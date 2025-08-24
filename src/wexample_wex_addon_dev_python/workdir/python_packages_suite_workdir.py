524f99126b87bfb62dc15487148771cafb7cd6ef
weeger <romain.weeger@gmail.com>
2025-08-24 18:25:43 +0200

    Base publish method

--- src/wexample_wex_addon_dev_python/workdir/python_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_python/workdir/python_packages_suite_workdir.py
@@ -6,9 +6,21 @@ from wexample_config.const.types import DictConfig
 from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
+from wexample_wex_addon_dev_python.workdir.python_package_workdir import PythonPackageWorkdir
 
 
 class PythonPackagesSuiteWorkdir(FrameworkPackageSuiteWorkdir):
+    def publish_suite(self):
+        # TODO
+        for package in self.get_packages():
+            self.io.log(f'Publishing package {package.get_project_name()}')
+
+    def get_packages(self) -> list[PythonPackageWorkdir]:
+        pip_dir = self.find_by_name(item_name='pip')
+        if pip_dir:
+            return pip_dir.get_children_list()
+        return []
+
     def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
         from wexample_filestate.config_option.children_filter_config_option import (
             ChildrenFilterConfigOption,
