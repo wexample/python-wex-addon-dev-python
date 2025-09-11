556313a2e5403f293e9ee6cc7e5d96d43cbbe1dd
weeger <romain.weeger@gmail.com>
2025-09-11 09:05:27 +0200

    Rect

--- src/wexample_wex_addon_dev_python/file/python_package_toml_file.py
+++ src/wexample_wex_addon_dev_python/file/python_package_toml_file.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_filestate.item.file.toml_file import TomlFile
+from wexample_helpers.decorator.base_class import base_class
 from wexample_wex_core.workdir.mixin.as_suite_package_item import (
     AsSuitePackageItem,
 )
 
-from wexample_helpers.decorator.base_class import base_class
 if TYPE_CHECKING:
     from tomlkit import TOMLDocument
     from wexample_wex_core.workdir.code_base_workdir import (
