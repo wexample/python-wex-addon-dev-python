59beee1da7ee5080638bc737be1ece9e8334b8da
weeger <romain.weeger@gmail.com>
2025-09-19 15:27:02 +0200

    Various rect

--- src/wexample_wex_addon_dev_python/workdir/python_workdir.py
+++ src/wexample_wex_addon_dev_python/workdir/python_workdir.py
@@ -215,7 +215,9 @@ class PythonWorkdir(CodeBaseWorkdir):
         )
 
     def _create_python_file_children_filter(self) -> ChildrenFileFactoryOption:
-        from wexample_filestate.option.children_filter_option import ChildrenFilterOption
+        from wexample_filestate.option.children_filter_option import (
+            ChildrenFilterOption,
+        )
         from wexample_filestate.const.disk import DiskItemType
         from wexample_filestate_python.file.python_file import PythonFile
         from wexample_filestate_python.option.python_option import PythonOption
