c9aa63fe9f2713eeb13c017dae558d9c0365bcc6
weeger <romain.weeger@gmail.com>
2025-10-25 09:04:58 +0200

    Rect

--- src/wexample_wex_addon_dev_python/file/python_package_toml_file.py
+++ src/wexample_wex_addon_dev_python/file/python_package_toml_file.py
@@ -212,6 +212,7 @@ class PythonPackageTomlFile(AsSuitePackageItem, TomlFile):
             toml_get_string_value,
             toml_sort_string_array,
         )
+
         from wexample_wex_addon_dev_python.const.package import (
             RUNTIME_DEPENDENCY_REMOVE_NAMES,
         )
